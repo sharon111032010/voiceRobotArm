a984d25c0d4d0bee8d4c16587db2734cea45ccb7
Sharon <163980735+sharon111032010@users.noreply.github.com>
2026-07-18 18:13:18 +0800

    成功轉換fun 使用 變數 改變機械手臂

--- arm.py
+++ arm.py
@@ -1,11 +1,14 @@
 # 匯入 套件 pyniryo 是  控制套件
 from pyniryo import *
 import time 
+
 robot = NiryoRobot("192.168.0.4")  # connect to Niryo arm by socket
 
 robot.calibrate_auto()  # 動作校正 找每個馬達的零點 /確認 關節位置 
 robot.update_tool() #更新 機械手臂上裝的是哪個爪子
-robot.move_to_home_pose() #回到預設安全手勢
+robot.clear_collision_detected()
+robot.move_to_home_pose() #回到預設安全手勢 斜直線 
+
 #規範 X  Y  Z可以移動的範圍
 def move_X(x:float):
 	if not (0.2<= x<=0.4):
@@ -25,46 +28,91 @@ def move_Z(z:float):
 	return z  
 
 
-init_pose=PoseObject(0.3, 0.0, 0.15, 0.0,0.1,0.1)
-
-#示範用法:
-#init_pose=PoseObject(move_X(0.3),move_Y(0.2),move_Z(0.15),0.0,1.57,0.0)
-robot.move_pose(init_pose)
-
-
-
-#pose_read = robot.get_pose()  # x = 0.2001, y = 0.1000, z = 0.2499 roll = 0.604, pitch = 1.570, yaw = 0.605
-# print(pose_read)
 x = 0.2  #
 y = 0.0  # 爪具遠近
-z = 0.3  # 爪具高度
+z = 0.15  # 爪具高度
 
+POINTX = 0.2
+POINTY = 0.0
+POINTZ = 0.15
 # 笛卡爾座標
+
 roll = 0.0   # 爪具自轉軸
 pitch = 1.57 # 爪具的上下軸
 yaw = 0.0
 
-print("release_with_tool")
-#robot.release_with_tool()
-print("move pose")
-#				        x      y    z
-robot.move_linear_pose(0.2  , 0.0, 0.15, 0.0, 1.57, 0.0)
-print("grasp_with_tool")
-robot.grasp_with_tool() # 控制爪具抓取
+# robot.move_linear_pose(x,y,z, roll, pitch,yaw)
+# robot.release_with_tool() #開爪子 
+# robot.grasp_with_tool() #關爪子 
+def moveOn(Mx=x,My=y,Mz=z):
+	robot.move_linear_pose(Mx,My,Mz,roll,pitch,yaw)
+def moveMz(Mz):
+	robot.move_linear_pose(x,y,Mz, roll, pitch,yaw)
+def moveMy(My):
+	robot.move_linear_pose(x,My,z, roll, pitch,yaw)
+def moveMx(Mx):
+	robot.move_linear_pose(Mx,y,z, roll, pitch,yaw)
 
-time.sleep(0.5)
-print("move pose")
-robot.move_linear_pose(0.2, 0.1, 0.15, 0.0, 1.57, 0.0)
-robot.release_with_tool() # 控制爪具釋放
+def close():
+	robot.grasp_with_tool()
+def open():
+	robot.release_with_tool()
 
-init_pose=PoseObject(0.3, 0.0, 0.15, 0.0,0.1,0.1)
-robot.move_pose(init_pose)
-
+# print(joints)
 
 # 這是原點座標
-time.sleep(0.5)
-robot.move_linear_pose(0.2  , 0.0, 0.15, 0.0, 1.57, 0.0)
-print("grasp_with_tool")
-robot.grasp_with_tool() # 控制爪具抓取
+time.sleep(0.1)
+''' 前後 左右 上下 '''
+# robot.move_linear_pose(0.2  , 0.0, 0.15, 0.0, 1.57, 0.0)
+def setSide():
+    global x, y, z
+    x = 0.2
+    y = 0.0
+    z = 0.15
+def go ():
+	robot.move_linear_pose(x,y,z, roll, pitch,yaw)
+def home ():
+	robot.move_linear_pose(POINTX,POINTY,POINTZ, roll, pitch,yaw)
+def catch ():
+	robot.release_with_tool() #開爪子 
+	#robot.move_linear_pose(0.2  , 0.0, 0.15, 0.0, 1.57, 0.0)
+	#print("向下 0.15")
 
+	robot.move_linear_pose(0.2  , 0.0, 0.10, 0.0, 1.57, 0.0)
+	print("向下 到10")
+	robot.grasp_with_tool()
+	print("上升到0.15")
+	robot.move_linear_pose(0.2  , 0.0, 0.15, 0.0, 1.57, 0.0)
+	robot.grasp_with_tool()
+def turnRight():
+	robot.move_linear_pose(0.2,0.1,0.15, 0.0,1.57,0.0)
+	robot.move_linear_pose(0.2,0.1,0.10, 0.0,1.57,0.0)
+def turnUp():
+	robot.move_linear_pose(0.2,0.1,0.13, 0.0,1.57,0.0)
+
+home()
+# moveMz(0.10)
+catch()
+# 放第四象限
+y=-0.1
+go()
+# openClaw()
+z=0.10
+go()
+open()
+z=0.15
+go()
+home()
+# 回中心
+
+
+
+
+
+# turnRight()
+# putdown()
+# turnUp()
+# close()
+# home()
+# close()
 robot.close_connection()
--- test.py
+++ test.py
@@ -26,24 +26,27 @@ def move_Z(z:float):
 	return z  
 
 
+
 init_pose=PoseObject(0.3, 0.0, 0.15, 0.0,0.1,0.1)
 
 #示範用法:
 #init_pose=PoseObject(move_X(0.3),move_Y(0.2),move_Z(0.15),0.0,1.57,0.0)
 robot.move_pose(init_pose)
+time.sleep(0.1)
 
-
+# robot.move(JointsPosition(0.1, -0.2, 0.0, 1.1, -0.5, 0.2)) // 絕對位置 在 相對 右上 角
 
 #pose_read = robot.get_pose()  # x = 0.2001, y = 0.1000, z = 0.2499 roll = 0.604, pitch = 1.570, yaw = 0.605
 # print(pose_read)
-x = 0.2  #
-y = 0.0  # 爪具遠近
-z = 0.3  # 爪具高度
+# x = 0.2  #
+# y = 0.0  # 爪具遠近
+# z = 0.3  # 爪具高度
 
-# 笛卡爾座標
-roll = 0.0   # 爪具自轉軸
-pitch = 1.57 # 爪具的上下軸
-yaw = 0.0
+# # 笛卡爾座標
+
+# roll = 0.0   # 爪具自轉軸
+# pitch = 1.57 # 爪具的上下軸
+# yaw = 0.0
 
 print("release_with_tool")
 robot.release_with_tool()
@@ -63,51 +66,51 @@ robot.grasp_with_tool() # 控制爪具抓取
 time.sleep(0.5)
 
 
-#夾取 下方正方形 
+# #夾取 下方正方形 
 
-# 取得目前位置
-pose = robot.get_pose()
+# # 取得目前位置
+# pose = robot.get_pose()
 
-x = pose.x
-y = pose.y
-z = pose.z
-
-
-# 先到物體上方 5 cm
-robot.move_linear_pose(
-    x,
-    y,
-    z-0.03,
-    0,
-    1.57,
-    0
-)
-
-# 慢慢下降到物體高度
-robot.move_linear_pose(
-    x,
-    y,
-    z-0.05,
-    0,
-    1.57,
-    0
-)
-
-# 關閉夾爪
-robot.grasp_with_tool()
-
-time.sleep(0.5)
+# x = pose.x
+# y = pose.y
+# z = pose.z
 
 
-# 抬起
-robot.move_linear_pose(
-    x,
-    y,
-    z-0.02,
-    0,
-    1.57,
-    0
-)
+# # 先到物體上方 5 cm
+# robot.move_linear_pose(
+#     x,
+#     y,
+#     z-0.03,
+#     0,
+#     1.57,
+#     0
+# )
 
+# # 慢慢下降到物體高度
+# robot.move_linear_pose(
+#     x,
+#     y,
+#     z-0.05,
+#     0,
+#     1.57,
+#     0
+# )
+
+# # 關閉夾爪
+# robot.grasp_with_tool()
+
+# time.sleep(0.5)
+
+
+# # 抬起
+# robot.move_linear_pose(
+#     x,
+#     y,
+#     z-0.02,
+#     0,
+#     1.57,
+#     0
+# )
+# robot.move_to_home_pose() #回到預設安全手勢 斜直線 
 
 robot.close_connection()
